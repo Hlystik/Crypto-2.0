45d47447c3a8609728ccd08bde1b078ef892794d
Anatoly <hlystike@rambler.ru>
2022-11-09 14:02:29 +0500

    done

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,22 @@
 $(function () {
-    /* $(document).ready(function() {
-        //прикрепляем клик по заголовкам acc-head
-          $('#accordeon .acc-head').on('click', f_acc);
-      });
-       
-      function f_acc(){
-      //скрываем все кроме того, что должны открыть
-        //$('#accordeon .acc-body').not($(this).next()).slideUp(500);
-        //$('.acc-head').removeClass('acc-head_active');
-      // открываем или скрываем блок под заголовком, по которому кликнули
-          $(this).next().slideToggle(500);
-          $(this).toggleClass('acc-head_active');
-      } */
-      $('.acc-head').on('click', function () {
-        $(this).toggleClass('acc-head_active').next().slideToggle();
-        $('.acc-head').not(this).removeClass('acc-head_active').next().slideUp();
-          });
-          $('#accordeon .acc-head:first').click();
+  $("a[href*='#']").on("click", function (e) {
+    const anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 777);
+    e.preventDefault();
+    return false;
+  });
+  //первый слайд
+  /* $('.acc-head').on('click', function (e) {
+    e.preventDefault();
+    $(this).next('.acc-body').slideToggle('.faq__accordeon');
+    $(this).toggleClass('acc-head_active');
+  }); */
+  //второй слайд
+  $('.acc-head').on('click', function () {
+    $(this).toggleClass('acc-head_active').next().slideToggle();
+    $('.acc-head').not(this).removeClass('acc-head_active').next().slideUp();
+  });
+  $('#accordeon .acc-head:first').click();
 });
